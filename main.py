ad92e0538a6f1741e62c5b08b9b1dcac8a7c6eab
xxxujie-wsl <wsl-docker-lab@lightfall.com>
2024-08-03 19:12:30 +0000

    优化 main 结构

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from common.utils import loggers
 
 
 def main():
+    logger.info("Start running")
+    pbn_generator.img2pbn(settings.DATA_DIR)
+
+
+if __name__ == "__main__":
     # 定义一个解析器，用于命令行执行时解析附带的参数
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_dir", type=str, default="")
     args = parser.parse_args()
     if args.config_dir != "":
         settings.USER_CONFIG_DIRS.insert(0, args.config_dir)
-
-
-if __name__ == "__main__":
-    main()
-
     logger = loggers.get_logger()
-    logger.info("Start running")
-    pbn_generator.img2pbn(settings.DATA_DIR)
+    main()
